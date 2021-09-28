68f3f89330f74fc2d456e7aa6e984274a7171ca7
benceb1 <bence.b1.1.2@gmail.com>
2021-09-28 10:39:21 +0200

    elm

--- ora3_kurzor_pozicionalas.asm
+++ ora3_kurzor_pozicionalas.asm
@@ -10,7 +10,10 @@ Start:
 Program_Vege:
 	mov ax, 4c00h
 	int 21h
-
+	
+	;kurzor mozg (kiiras elott kell meghivni, es erdemes elotte egy clr)
+	; 10h megszakitas -> 02 funkcio (ah), xy koordinatak (dh, dl), melyik video page (nem lapozgatunk, szoval bh-ba 0-t toltunk) 
+	
 Code	Ends
 
 Data	Segment
